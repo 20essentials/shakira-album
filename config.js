2db4077538a545bad3d0e3d117de22ccb4d501a2
DominicDev <s0l0estudi4r@gmail.com>
2026-04-03 20:28:32 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'SHAKIRA';
+export let nameSongs = [
+	"TRY EVERYTHING", /*1*/
+	"WHENEVER, WHEREVER", /*2*/
+	"SHE WOLF", /*3*/
+	"HIPS DON'T LIE", /*4*/
+	"ME ENAMORÉ", /*5*/
+	"WAKA WAKA (THIS TIME FOR AFRICA)", /*6*/
+	"LOCA", /*7*/
+	"CHANTAJES", /*8*/
+	"OBJECTION (TANGO", /*9*/
+	"EMPIRE", /*10*/
+	"CAN'T REMEMBER TO FORGET YOU", /*11*/
+	"TORTURA", /*12*/
+	"NADA", /*13*/
+	"CIEGA, SORDOMUDA", /*14*/
+	"PIES DESCALZOS, SUEÑOS BLANCOS", /*15*/
+	"OJOS ASÍ", /*16*/
+	" CIEGA, SORDOMUDA", /*17*/
+	"SUERTE", /*18*/
+	"LA LA LA (BRAZIL 2014)", /*19*/
+	"ADDICTED TO YOU", /*20*/
+]
